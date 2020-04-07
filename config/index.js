879f9ad6a17e0d51ab728a7d63617f52bdd5eb29
Chandu J S <app.coder@outlook.in>
2020-04-07 21:04:18 +0530

    labels renamed

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
 module.exports = {
   pathPrefix: '/', 
 
-  siteTitle: 'Jodie',
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio',
-  siteTitleShort: 'Jodie',
-  siteHeadline: 'Come & Enjoy our excellent photos',
-  siteUrl: 'https://jodie.lekoarts.de',
+  siteTitle: 'Siva S.R.',
+  siteTitleAlt: 'Siva S.R. - Photohrapher',
+  siteTitleShort: 'Siva',
+  siteHeadline: 'Siva S.R. - Photohrapher',
+  siteUrl: 'https://sivasr.com',
   siteLanguage: 'en',
   siteLogo: '/logos/logo.png',
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts',
+  siteDescription: 'Siva S.R. - Photohrapher',
+  author: 'Chandu J S',
   themeColor: '#db7436',
   backgroundColor: '#3b3c4f'
 }
