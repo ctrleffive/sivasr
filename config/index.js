03230ed1c3c12e1024010b74557ec8a883a95ad8
Chandu J S <app.coder@outlook.in>
2020-04-07 20:56:32 +0530

    other files removed

--- config/index.js
+++ config/index.js
@@ -1,23 +1,15 @@
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  pathPrefix: '/', 
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteTitle: 'Jodie',
+  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio',
+  siteTitleShort: 'Jodie',
+  siteHeadline: 'Come & Enjoy our excellent photos',
+  siteUrl: 'https://jodie.lekoarts.de',
+  siteLanguage: 'en',
+  siteLogo: '/logos/logo.png',
   siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
-
-  // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
-  ogLanguage: 'en_US', // og:language
-  googleAnalyticsID: 'UA-XXXXXX-X',
-
-  // Manifest and Progress color
+  author: 'LekoArts',
   themeColor: '#db7436',
-  backgroundColor: '#3b3c4f',
+  backgroundColor: '#3b3c4f'
 }
